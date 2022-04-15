5e0357580c7a9109968ec37b814f22fe1cb27152
njorogegatua <gatuapnjoroge@gmail.com>
2022-04-15 03:42:41 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 
-  /*
-   * while (i < 10)
-   *{
-   *putchar(i);
-   *}
-   */
+while (i < 10)
+{
+putchar(i);
+}
+
 printf("infinte loop avoided! \\o/\n");
 return (0);
 }
