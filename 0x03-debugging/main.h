874e7602e0aec8528e88cb6f8025bcf09b155b4e
njorogegatua <gatuapnjoroge@gmail.com>
2022-04-15 02:59:59 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#idndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_nehative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void priny_times_table(int n);
+
+#endif
