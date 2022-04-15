056b2f1766e3cd61c561d22f7b836339eda9db4f
njorogegatua <gatuapnjoroge@gmail.com>
2022-04-15 06:13:33 -0700

    task 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 
-int i;
+         int i;
+	
+         printf("infinite loop incoming :(/n");
 
-printf("infinite loop incoming :(\n");
-
-i = 0;
-
+	 i = 0;
+	 
 /* while (i < 10)
  * {
  * putchar(i);
