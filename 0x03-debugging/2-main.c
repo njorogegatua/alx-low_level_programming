2f4ac66e84669332e8c312b7eac12a93c8f83bb6
njorogegatua <gatuapnjoroge@gmail.com>
2023-02-23 11:45:09 -0800

    lagest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,7 +2,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
 * main - prints the largest of 3 integers
 * Return: 0
 */
@@ -21,24 +20,4 @@ int main(void)
         printf("%d is the largest number\n", largest);
 
         return (0);
-=======
- * main - prints the largest of 3 integers
- * Return: 0
- */
-
-int main(void)
-{
-	int a, b, c;
-	int largest;
-
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
-
-	return (0);
->>>>>>> 793755d28ab99909e55f7c1d455315a1adc07d5a
 }
