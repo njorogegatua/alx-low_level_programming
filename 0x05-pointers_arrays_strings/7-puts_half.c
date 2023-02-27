7d589b0fbcf3a0e62f8a10a4aef2e4ed6cfdf038
njorogegatua <102298471+njorogegatua@users.noreply.github.com>
2023-02-27 19:44:12 +0300

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints only the second half of a string
+ * @str: is a character
+ * Return:void
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	/* get the length of the string */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	/* check if length is even or odd */
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len / 2) - 1;
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
