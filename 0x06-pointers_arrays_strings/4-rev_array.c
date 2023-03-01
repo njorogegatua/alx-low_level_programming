35ae99a850b6ae18558626192c9242a12389a938
njorogegatua <gatuapnjorge@gmail.com>
2023-03-01 09:42:45 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: is a pointer to integer type
+ * @n: is an integer
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
+}
