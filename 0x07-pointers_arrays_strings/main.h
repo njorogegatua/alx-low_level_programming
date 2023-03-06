1d95cb8534bb11feb80c293ea9cccc35f6bf37f0
njorogegatua <gatuapnjorge@gmail.com>
2023-03-06 06:28:58 -0800

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#define _MAIN_H_
 #ifndef _MAIN_H_
+#define _MAIN_H_
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -8,8 +8,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _putchar(char c)
 
-#end if
+#endif
