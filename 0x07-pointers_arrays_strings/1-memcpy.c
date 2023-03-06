3aab058d6b313d26c18b10342b4cfec37e27da3e
njorogegatua <gatuapnjorge@gmail.com>
2023-03-06 05:59:49 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  *_memcpy - copies the memory area
- * @dest:  a pointer to char
+ * @dest: is  a pointer to char
  * @src:is the memory area
  * @n: an unsigned int
  * Return: pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
