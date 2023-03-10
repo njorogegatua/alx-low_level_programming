68ff4447f08e3480d5a9f6f0f72a4f2805eee55c
njorogegatua <gatuapnjorge@gmail.com>
2023-03-10 09:39:48 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two no.s
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0) SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 2 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
