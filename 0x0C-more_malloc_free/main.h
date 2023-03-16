3a1743b83584e56f8b950c603e37d7b9d5ebceae
njorogegatua <gatuapnjorge@gmail.com>
2023-03-16 09:28:11 -0700

    task 2

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
