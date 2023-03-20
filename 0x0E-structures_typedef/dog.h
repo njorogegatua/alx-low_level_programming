9da5708586a59e08bc52f6a0d30d58c211e9b2ae
njorogegatua <gatuapnjorge@gmail.com>
2023-03-20 14:18:37 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,25 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - Struct dog
- * @name: name
- * @age: age
- * @owner:  owner
- * Description: This struct is for dawg
+ * struct dog - defines diff data types
+ * @name: pointer to a char that stores the address of the string literal
+ * @owner: pointer to a char that stores the address of the string literal
+ * @age: float
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
-	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+	float age;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /*DOG_H*/
-
+#endif
